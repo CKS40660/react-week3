6ed2dc914139da57768092d4212d3ec77c91027c
CKS40660 <118961951+CKS40660@users.noreply.github.com>
2026-01-15 10:13:34 +0800

    修正路徑

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   // 分成開發跟產品兩個路徑
-  base: process.env.NODE_ENV === "production" ? "/react-week2/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/react-week3/" : "/",
   plugins: [react()],
 });
